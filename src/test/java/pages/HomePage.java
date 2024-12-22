37f6052261a9f7491fba01fb6bb531979aa0b1b7
Gizem Yılmaz <yilmazgizemmm@outlook.com>
2024-12-23 00:20:14 +0300

    Updated project

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,10 +1,15 @@
 package pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     WebDriver driver;
@@ -15,13 +20,38 @@ public class HomePage {
         this.driver = driver;
     }
 
+    public void goToUrl(String url) {
+        // ChromeOptions ile tarayıcı ayarları
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+
+        // WebDriverManager ile tarayıcı sürümünü eşleştir
+        WebDriverManager.chromedriver().browserVersion("131.0").setup();
+
+        // WebDriver'i başlat
+        driver = new ChromeDriver(options);
+        driver.get(url);
+
+        // Tarayıcıyı tam ekran yap
+        driver.manage().window().maximize();
+    }
+
     // Çerez kabul etme metodu
     public void acceptCookies() {
         try {
-            WebElement acceptCookiesButton = driver.findElement(By.id("onetrust-accept-btn-handler"));
+            // WebDriverWait ile maksimum 10 saniye bekler
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 10 saniye bekleme süresi
+
+            // Elementi bekler ve bulunduğunda döner
+            WebElement acceptCookiesButton = wait.until(
+                    ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='onetrust-accept-btn-handler' and text()='Kabul et']"))
+            );
+
+            // Bulunan elementi tıkla
             acceptCookiesButton.click();
+            System.out.println("Cookie accept button clicked.");
         } catch (Exception e) {
-            System.out.println("Çerez kabul etme butonu bulunamadı: " + e.getMessage());
+            System.out.println("Cookie accept button not found: " + e.getMessage());
         }
     }
 
--- src/test/java/stepdefinitions/UI_Steps.java
+++ src/test/java/stepdefinitions/UI_Steps.java
@@ -1,5 +1,6 @@
 package stepdefinitions;
 
+import io.cucumber.java.After;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -19,17 +20,15 @@ import java.util.List;
 public class UI_Steps {
     WebDriver driver;
     String productPrice;
-    HomePage homePage; // HomePage instance
+    HomePage homePage;
 
+    public UI_Steps() {
+        homePage = new HomePage(driver);
+    }
 
     @Given("Kullanıcı {string} adresine gider")
     public void kullanıcı_adresine_gider(String url) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-        driver.get(url);
-        driver.manage().window().maximize();
+        homePage.goToUrl(url);
 
 
     }
@@ -37,7 +36,7 @@ public class UI_Steps {
 
     @Given("Kullanıcı çerezleri kabul eder")
     public void kullanıcı_çerezleri_kabul_eder() {
-        HomePage homePage = new HomePage(driver); // HomePage nesnesi oluşturulur
+        HomePage homePage = new HomePage(driver);
         homePage.acceptCookies();
 
 
@@ -48,24 +47,27 @@ public class UI_Steps {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         Actions actions = new Actions(driver);
 
-
-        WebElement elektronik = driver.findElement(By.xpath("//span[contains(text(), '" + kategori + "')]"));
+        WebElement elektronik = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),, '"+ kategori + "']")));
         actions.moveToElement(elektronik).perform();
+        homePage.waitForSeconds(10000);
 
-        //homePage.waitForElementToBeVisible(By.xpath("//a[text()='" + altKategori + "']"));
         WebElement bilgisayar = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='" + altKategori + "']")));
         actions.moveToElement(bilgisayar).perform();
+        homePage.waitForSeconds(10000);
 
         WebElement apple = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(), '" + altaltKategori + "')]")));
         actions.moveToElement(apple).perform();
         apple.click();
         homePage.waitForSeconds(10000);
+
+
     }
+
 
     @When("Filtrelerden {string} seçer")
     public void filtrelerden_seçer() {
-        //WebElement button = driver.findElement(By.xpath("//a[contains(@class, 'moria-PillButton') and contains(text(), '13,2 inç')]"));
-        //button.click();
+        WebElement button = driver.findElement(By.xpath("//*[@id='HeroFilter_2311f3f4-4702-4778-0fbf-11dc4a688194']//a[@class='moria-PillButton-BNQ0U shbxLs8byorrdfvi heroContent-IuUq0v8_wAueO8ELIHZ5']//span[contains(text(),'13,2')]"));
+        button.click();
         homePage.waitForSeconds(10000);
         driver.navigate().refresh();
 
@@ -73,10 +75,14 @@ public class UI_Steps {
 
     @When("Kullanıcı çıkan sonuçlardan en yüksek fiyatlı ürüne tıklar")
     public void kullanıcı_en_yüksek_fiyatlı_ürüne_tıklar() {
+        // create a list with the prices of the products that are listed
         List<WebElement> prices = driver.findElements(By.cssSelector("div[data-test-id='price-current-price']"));
-        WebElement highestPriceElement = prices.get(0);
+        // here find the highest price of the product
+        WebElement highestPriceElement = prices.getFirst();
         highestPriceElement.click();
         productPrice = driver.findElement(By.cssSelector("div[data-test-id='price-current-price']")).getText();
+
+
     }
 
     @When("Kullanıcı açılan ürün detay sayfasındaki Sepete Ekle butonuna tıklar")
@@ -88,10 +94,21 @@ public class UI_Steps {
 
     @Then("Ürünün sepete eklendiğini ve fiyatının ürün detay sayfasıyla aynı olduğunu doğrular")
     public void ürünü_sepeti_doğrular() {
-        CartPage card = new CartPage(driver); // Sayfa nesnesini oluştur
+        CartPage card = new CartPage(driver);
         card.GoToCart();
+        //save the price of the product
         String basketPrice = driver.findElement(By.className("product_price_uXU6Qb")).getText();
+        //compare the prices of the same product on the product page & on the card page
         assert productPrice.equals(basketPrice) : "Fiyatlar eşleşmiyor!";
         driver.quit();
+
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Bye Bye!");
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
